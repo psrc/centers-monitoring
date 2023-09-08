c286ceb1afc2aa91488ed034cdcb3c4d0cb7f3dd
Craig Helmann <chelmann@psrc.org>
2023-09-08 10:51:05 -0700

    resident mode share removed for MIC's

--- modules/transportation_tab.R
+++ modules/transportation_tab.R
@@ -15,15 +15,19 @@ transportation_server <- function(id, center_name, center_type, center_desc) {
     ns <- session$ns
     
     # Tables and Charts
-    output$resident_mode_chart <- renderEcharts4r({
-      
-      echart_multi_column_chart(df = mode_data %>% 
-                                  filter(geography_type %in% c(center_type, "County") & geography %in% c(center_name(), "Region", "All Centers") & grouping != "Total"),
-                                x = "grouping", y = "share", fill="geography", tog = "data_year", 
-                                dec = 0, esttype = "percent", color = "jewel")
-    })
     
-    output$resident_mode_table <- DT::renderDataTable({create_multi_year_table(df = mode_data, rgc_name = center_name(), data_yrs = as.character(census_years), dec = 1, center_type = center_type)})
+    if (center_type == rgc_title) {
+      
+      output$resident_mode_chart <- renderEcharts4r({
+        echart_multi_column_chart(df = mode_data %>% 
+                                    filter(geography_type %in% c(center_type, "County") & geography %in% c(center_name(), "Region", "All Centers") & grouping != "Total"),
+                                  x = "grouping", y = "share", fill="geography", tog = "data_year", 
+                                  dec = 0, esttype = "percent", color = "jewel")
+        })
+    
+      output$resident_mode_table <- DT::renderDataTable({create_multi_year_table(df = mode_data, rgc_name = center_name(), data_yrs = as.character(census_years), dec = 1, center_type = center_type)})
+      
+    }
     
     output$destination_mode_chart <- renderEcharts4r({
       
@@ -42,38 +46,68 @@ transportation_server <- function(id, center_name, center_type, center_desc) {
     # Tab layout
     output$atransportationtab <- renderUI({
       
-      tagList(
+      if (center_type == rgc_title) {
+      
+        tagList(
         
-        hr(),
+          hr(),
         
-        # Transit Stop Map and Table
-        fluidRow(column(6, leafletOutput(ns("stop_map"))),
-                 column(6, strong("Transit Service"),
-                        br(),
-                        dataTableOutput(ns("stop_table")),
-                        br(),
-                        tags$div(class="chart_source","Source: Spring GTFS Service")
-                 )),
-        fluidRow(column(12, div(img(src="transit-legend.png", width = "75%", style = "padding-left: 0px;")))),
-        br(),
+          # Transit Stop Map and Table
+          fluidRow(column(6, leafletOutput(ns("stop_map"))),
+                  column(6, strong("Transit Service"),
+                          br(),
+                          dataTableOutput(ns("stop_table")),
+                          br(),
+                          tags$div(class="chart_source","Source: Spring GTFS Service")
+                  )),
+          fluidRow(column(12, div(img(src="transit-legend.png", width = "75%", style = "padding-left: 0px;")))),
+         br(),
         
-        # Resident Mode Share
-        strong(tags$div(class="chart_title","Mode to Work for Residents")),
-        fluidRow(column(12,echarts4rOutput(ns("resident_mode_chart")))),
-        br(),
-        fluidRow(column(12, dataTableOutput(ns("resident_mode_table")))),
-        tags$div(class="chart_source","Source: US Census Bureau American Community Survey (ACS) 5yr Data Table B08301"),
-        hr(style = "border-top: 1px solid #000000;"),
+          # Resident Mode Share
+          strong(tags$div(class="chart_title","Mode to Work for Residents")),
+          fluidRow(column(12,echarts4rOutput(ns("resident_mode_chart")))),
+          br(),
+          fluidRow(column(12, dataTableOutput(ns("resident_mode_table")))),
+          tags$div(class="chart_source","Source: US Census Bureau American Community Survey (ACS) 5yr Data Table B08301"),
+          hr(style = "border-top: 1px solid #000000;"),
           
         
-      # Destination Mode Share
-        strong(tags$div(class="chart_title","Destination Mode Share")),
-        fluidRow(column(12,echarts4rOutput(ns("destination_mode_chart")))),
-        br(),
-        fluidRow(column(12, dataTableOutput(ns("destination_mode_table")))),
-        tags$div(class="chart_source","Source: 2018 PSRC SoundCast Activity Based Model"),
-        hr(style = "border-top: 1px solid #000000;")
-      )
+          # Destination Mode Share
+          strong(tags$div(class="chart_title","Destination Mode Share")),
+          fluidRow(column(12,echarts4rOutput(ns("destination_mode_chart")))),
+          br(),
+          fluidRow(column(12, dataTableOutput(ns("destination_mode_table")))),
+          tags$div(class="chart_source","Source: 2018 PSRC SoundCast Activity Based Model"),
+          hr(style = "border-top: 1px solid #000000;")
+        )
+        
+      } else {
+        
+        tagList(
+          
+          hr(),
+          
+          # Transit Stop Map and Table
+          fluidRow(column(6, leafletOutput(ns("stop_map"))),
+                   column(6, strong("Transit Service"),
+                          br(),
+                          dataTableOutput(ns("stop_table")),
+                          br(),
+                          tags$div(class="chart_source","Source: Spring GTFS Service")
+                   )),
+          fluidRow(column(12, div(img(src="transit-legend.png", width = "75%", style = "padding-left: 0px;")))),
+          br(),
+          
+          # Destination Mode Share
+          strong(tags$div(class="chart_title","Destination Mode Share")),
+          fluidRow(column(12,echarts4rOutput(ns("destination_mode_chart")))),
+          br(),
+          fluidRow(column(12, dataTableOutput(ns("destination_mode_table")))),
+          tags$div(class="chart_source","Source: 2018 PSRC SoundCast Activity Based Model"),
+          hr(style = "border-top: 1px solid #000000;")
+        )
+        
+      }
       
     })
     
